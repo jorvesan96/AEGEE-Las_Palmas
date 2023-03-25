1c52128deee933a62a1580c4339c1d980d6a2536
AnaSantana016 <72494472+AnaSantana016@users.noreply.github.com>
2023-03-25 14:24:53 +0100

    Casi terminada la validacion de perfil

---
+++ Perfil/validacionPerfil.js
@@ -0,0 +1,102 @@
+//Expresiones regulares para la validación
+const expresiones = {
+	pais:  /^[a-zA-Z]{2,50}$/ ,
+    ciudad: /^[a-zA-Z ]+$/ ,
+    codigoPostal: /^(?:0[1-9]|[1-4]\d|5[0-2])\d{3}$/ ,
+	direccion: /^[a-zA-Z0-9\s.,#-]+$/ , 
+    telefono: /^\(?(\d{3})\)?(\d{3})(\d{3})$/ ,
+    dni: /^\d{8}[a-zA-Z]$/ ,
+	email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+}
+
+document.getElementById("pais").addEventListener("blur", function () {
+    if (!expresiones.pais.test(pais.value)) {
+      alerta("El campo País solo permite letras.", "rojo");
+      pais.classList.add("invalid");
+    } else {
+      pais.classList.remove("invalid");
+    }
+});
+  
+document.getElementById("ciudad").addEventListener("blur", function () {
+    if (!expresiones.ciudad.test(ciudad.value)) {
+        alerta("El campo Ciudad solo permite letras.", "rojo");
+        ciudad.classList.add("invalid");
+    } else {
+        ciudad.classList.remove("invalid");
+    }
+});
+
+document.getElementById("postal").addEventListener("blur", function () {
+    if (!expresiones.codigoPostal.test(postal.value)) {
+        alerta("El campo Codigo Postal es inválido.", "rojo");
+        postal.classList.add("invalid");
+    } else {
+        postal.classList.remove("invalid");
+    }
+});
+
+document.getElementById("direccion").addEventListener("blur", function () {
+    if (!expresiones.direccion.test(direccion.value)) {
+        alerta("El campo Direccion solo permite letras y espacios.", "rojo");
+        direccion.classList.add("invalid");
+    } else {
+        direccion.classList.remove("invalid");
+    }
+});
+
+document.getElementById("telefono").addEventListener("blur", function () {
+    if (!expresiones.telefono.test(telefono.value)) {
+        alerta("El campo Telefono solo permite números.", "rojo");
+        telefono.classList.add("invalid");
+    } else {
+        telefono.classList.remove("invalid");
+    }
+});
+
+document.getElementById("dni").addEventListener("blur", function () {
+    if (!expresiones.dni.test(dni.value)) {
+        alerta("El campo DNI solo permite números y una letra.", "rojo");
+        dni.classList.add("invalid");
+    } else {
+        dni.classList.remove("invalid");
+    }
+});
+
+document.getElementById("email").addEventListener("blur", function () {
+    if (!expresiones.email.test(email.value)) {
+        alerta("El campo Email solo permite letras.", "rojo");
+        email.classList.add("invalid");
+    } else {
+        email.classList.remove("invalid");
+    }
+});
+  
+function alerta(mensaje, color) {
+    const div = document.createElement("div");
+    div.classList.add("alerta");
+    div.classList.add(color);
+    div.textContent = mensaje;
+    const formulario = document.querySelector("form");
+    formulario.appendChild(div);
+    setTimeout(function () {
+      div.remove();
+    }, 3000);
+}
+
+function validarInput(input, expresion, mensaje) {
+    if (!expresion.test(input.value)) {
+        input.classList.add("invalid");
+        const span = input.parentElement.querySelector(".error");
+        span.textContent = mensaje;
+        span.style.display = "block";
+        alerta(mensaje, "error"); // llamada a la función alerta en caso de error
+        return false;
+    } else {
+        input.classList.remove("invalid");
+        const span = input.parentElement.querySelector(".error");
+        span.textContent = "";
+        span.style.display = "none";
+        return true;
+    }
+}
