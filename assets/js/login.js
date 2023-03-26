5f4eade5eb70f3b3165761baa61553c6f2547efc
AnaSantana016 <72494472+AnaSantana016@users.noreply.github.com>
2023-03-26 14:04:43 +0200

    Para que cambiara el header que en vez de poner acceder pusiera perfil

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
-    const boton = document.getElementById('acceso');
+    const boton = document.getElementById("acceso");
 
-    boton.addEventListener('click' , function() {
-        var aux = true;
-
-        //Obtengo los valores ingresados por el usuario
-        const correo = document.querySelector('input[name="correo"]').value;
-        const contraseña = document.querySelector('input[name="contraseña"]').value;
-        console.log(correo);
+    boton.addEventListener("click" , function() {
         
         //Cargar el archivo JSON de usuario y convertirlo en un objeto JavaScript
         fetch('assets/json/user.json')
@@ -18,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
             console.log(usuario);
 
             //Agrego un evento 'submit' al formulario
-            document.querySelector('form').addEventListener('onclik', (event) => {
+            document.querySelector('form').addEventListener("onclik", (event) => {
                 //Prevenir que se envíe el formulario
                 event.preventDefault();
 
@@ -30,6 +24,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 if (correo != usuario.correo_electronico && contraseña != usuario.contraseña) {
                     //Guardo el objeto de usuario en el almacenamiento local del navegador
                     localStorage.setItem('usuarioActual', JSON.stringify(usuario));
+                    
+                    //Remuevo el botón de "Accede a tu cuenta" y agrego el botón de perfil
+                    const botonAcceder = document.getElementById("btn-acceder");
+                    const padreBotonAcceder = botonAcceder.parentNode;
+                    const botonPerfil = document.createElement('a');
+                    botonPerfil.textContent = 'Mi perfil';
+                    botonPerfil.href = './Perfil.Perfil.html';
+                    botonPerfil.classList.add('boton_header');
+                    padreBotonAcceder.removeChild(botonAcceder);
+                    padreBotonAcceder.appendChild(botonPerfil);
 
                     //Redirijo al usuario a la página de inicio
                     window.location.assign('/Home/Home.html');
@@ -41,3 +45,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
         });
     });
 });
+
