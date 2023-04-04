f738dfb9a090e83adee6c2c5b0b09d84a7626a0d
AlejandroOrtegaG <alejandroog99@gmail.com>
2023-04-04 10:43:56 +0100

    Header creado

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener,  } from '@angular/core';
 
 @Component({
   selector: 'components-header',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  public getScreenWidth: any;
+  public getScreenHeight: any;
+  public source: String = "../../../assets/icons/burger menu.png";
 
+  isOpen = false;
+
+  ngOnInit() {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
+  }
+
+  @HostListener('window:resize', ['$event'])
+
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
+  }
+
+  toggleMenu(): void {
+    this.isOpen = !this.isOpen
+    if (this.isOpen){
+      this.source = "../../../assets/icons/close-logo.png";
+    } else {
+      this.source = "../../../assets/icons/burger menu.png";
+    }
+  }
 }
