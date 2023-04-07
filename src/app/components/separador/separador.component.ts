ce5ce6e6770779439f799463c7bf2811b2b7c3ab
AlejandroOrtegaG <alejandroog99@gmail.com>
2023-04-07 16:33:56 +0100

    Componente separador

---
+++ src/app/components/separador/separador.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'components-separador',
+  templateUrl: './separador.component.html',
+  styleUrls: ['./separador.component.css']
+})
+export class SeparadorComponent {
+
+}
