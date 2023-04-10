501fe6c15a2d2f47cc17da196d2bea9980c6ce31
AnaSantana016 <72494472+AnaSantana016@users.noreply.github.com>
2023-04-10 09:17:56 +0200

    Rutas de la página hechas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,61 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { ActividadesComponent } from './pages/actividades/actividades.component';
+import { BuddyProgramComponent } from './pages/buddy-program/buddy-program.component';
+import { CreadorActividadesComponent } from './pages/creador-actividades/creador-actividades.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InicioSesionComponent } from './pages/inicio-sesion/inicio-sesion.component';
+import { MisActividadesComponent } from './pages/mis-actividades/mis-actividades.component';
+import { PerfilUsuarioComponent } from './pages/perfil-usuario/perfil-usuario.component';
+import { RecepcionDocumentosComponent } from './pages/recepcion-documentos/recepcion-documentos.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { VoluntariadoComponent } from './pages/voluntariado/voluntariado.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'actividades',
+    component: ActividadesComponent
+  },
+  {
+    path: 'buddy-program',
+    component: BuddyProgramComponent
+  },
+  {
+    path: 'creador-actividades',
+    component: CreadorActividadesComponent
+  },
+  {
+    path: 'inicio-sesion',
+    component: InicioSesionComponent
+  },
+  {
+    path: 'mis-actividades',
+    component: MisActividadesComponent
+  },
+  {
+    path: 'perfil-usuario',
+    component: PerfilUsuarioComponent
+  },
+  {
+    path: 'recepcion-documentos',
+    component: RecepcionDocumentosComponent
+  },
+  {
+    path: 'registro',
+    component: RegistroComponent
+  },
+  {
+    path: 'voluntariado',
+    component: VoluntariadoComponent
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
