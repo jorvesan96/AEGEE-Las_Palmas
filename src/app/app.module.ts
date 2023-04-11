6af4d76cfbd13e7f11411bde820d50b68c4e2512
AlejandroOrtegaG <alejandroog99@gmail.com>
2023-04-11 12:25:09 +0100

    Validacion Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,7 +37,8 @@ import { SeparadorComponent } from './components/separador/separador.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/inicio-sesion/inicio-sesion.component.ts
+++ src/app/pages/inicio-sesion/inicio-sesion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'pages-inicio-sesion',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class InicioSesionComponent {
 
+  form = new FormGroup({
+    correo: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    ])),
+    contraseña: new FormControl('', Validators.compose([
+      Validators.minLength(5),
+      Validators.required,
+      Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$') //this is for the letters (both uppercase and lowercase) and numbers validation
+    ])),
+  });
+
+  get f() {
+    return this.form.controls;
+  }
+
+  login() {
+    console.log(this.form.value);
+  }
 }
