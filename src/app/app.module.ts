9a0a0fbc3e5ff1ba3fdc34cdc6726a4d5715001c
AnaSantana016 <72494472+AnaSantana016@users.noreply.github.com>
2023-04-13 12:57:01 +0200

    intento de que funcione escribir en el json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +39,8 @@ import { SeparadorComponent } from './components/separador/separador.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-registro',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class RegistroComponent {
 
+  usuario = {
+    nombre: '',
+    apellidos: '',
+    dni: '',
+    fecha: '',
+    pais: '',
+    localidad: '',
+    estudios: '',
+    universidad: '',
+    carrera: '',
+    telefono: '',
+    correo: '',
+    contraseña: ''
+  }
+
+  constructor(private http: HttpClient) {}
+
+  registrar() {
+    this.http.post('assets/json/usuario.json', this.usuario)
+      .subscribe(
+        (data) => {
+          console.log('Registro exitoso', data);
+        },
+        (error) => {
+          console.log('Error en el registro', error);
+        }
+      );
+  }
 }
