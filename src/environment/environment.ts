0fbd8b430f2c7fa638e863e124b71eca8cfb1d9d
AlejandroOrtegaG <alejandroog99@gmail.com>
2023-04-22 12:35:49 +0100

    importar firebase

---
+++ src/environment/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+
+    apiKey: "AIzaSyA-GoDukZ1p1SpAmQ_j6cKEBSxZCc37jf0",
+    authDomain: "aegee-las-palmas.firebaseapp.com",
+    databaseURL:"https://aegee-las-palmas-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "aegee-las-palmas",
+    storageBucket: "aegee-las-palmas.appspot.com",
+    messagingSenderId: "425511324657",
+    appId: "1:425511324657:web:60202ffe07bd9d6cb95250",
+    measurementId: "G-7DSD2W6KGQ"
+  },
+  production: false
+};
