b8efea123d300e7518aaf55b6d4a13eda20575c9
AlejandroOrtegaG <alejandroog99@gmail.com>
2023-04-25 19:50:37 +0100

    añadidos metodos para autenticar usuario

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Auth, authState, signOut } from '@angular/fire/auth';
+import { take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-
-  constructor(private _auth: AngularFireAuth) { }
+  constructor(private _auth: AngularFireAuth, private user: Auth) { }
 
   signInWithEmail(email: string, password: string) {
     return this._auth.signInWithEmailAndPassword(email, password);
@@ -26,5 +27,23 @@ export class AuthService {
           return false;
         }
       });
+  }
+
+  isLogged(): boolean {
+    // this.user.onAuthStateChanged((_user) => {
+    //   if (_user) {
+    //     console.log('El usuario está autenticado:', _user);
+    //     return true;
+    //   } else {
+    //     console.log('El usuario no está autenticado');
+    //     return false;
+    //   }
+    // });
+
+    this._auth.authState.pipe(take(1)).subscribe(_user => {
+      console.log(_user);
+    });
+
+    return false;
   }
 }
