43523a26afbde5b0e780aa9cbef7ce5abf7c39b2
AlejandroOrtegaG <alejandroog99@gmail.com>
2023-04-26 18:29:29 +0100

    Template contraseña olvidada

--- src/app/pages/contrasena-olvidada/contrasena-olvidada.component.ts
+++ src/app/pages/contrasena-olvidada/contrasena-olvidada.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-contrasena-olvidada',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class ContrasenaOlvidadaComponent {
 
+  constructor (private authService: AuthService){}
+
+  correo: string | undefined;
+
+  olvidadaForm = new FormGroup({
+    correo: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    ])),
+  });
+
+  get f() {
+    return this.olvidadaForm.controls;
+  }
+
+  enviarCodigo(olvidadaForm: any): void {
+
+    return this.authService.userExists(olvidadaForm.correo);
+  }
 }
