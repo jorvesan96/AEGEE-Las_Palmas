d2e2d1eb5ba8e325b41d76eb6a388c551c681129
AnaSantana016 <72494472+AnaSantana016@users.noreply.github.com>
2023-05-06 11:46:58 +0200

    Oficialmente se puede modificar los campos de perfil

--- src/app/pages/perfil-usuario/perfil-usuario.component.ts
+++ src/app/pages/perfil-usuario/perfil-usuario.component.ts
@@ -3,6 +3,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Usuario } from 'src/app/services/usuario'
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { FirestoreService } from 'src/app/services/firestore.service';
+
 @Component({
   selector: 'pages-perfil-usuario',
   templateUrl: './perfil-usuario.component.html',
@@ -25,45 +26,15 @@ export class PerfilUsuarioComponent {
     localidad: ''
   };
 
+  editando = false;
+  textoBoton: string = 'Editar perfil';
+
   userUID!: string;
   user: any;
 
   constructor(private afAuth: AngularFireAuth, private firestore: AngularFirestore, private firestoreService: FirestoreService) { }
 
   ngOnInit() {
-
-    addEventListener('DOMContentLoaded', () => {
-      const btnEditarPerfil: HTMLElement | null = document.getElementById('editarPerfil');
-      const inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('input');
-      // Variable que indica si se están editando los campos o no
-      let editando: boolean = false;
-
-      if (btnEditarPerfil) {
-        btnEditarPerfil.addEventListener('click', () => {
-          // Cambia el valor de la variable cada vez que se hace clic en el botón
-          editando = !editando;
-
-          if (editando) {
-            // Cambia el texto del botón a "Guardar cambios"
-            if (btnEditarPerfil.textContent) {
-              btnEditarPerfil.textContent = 'Guardar cambios';
-            }
-            inputs.forEach((input: HTMLInputElement) => {
-              input.readOnly = false;
-            });
-          } else {
-            // Cambia el texto del botón a "Editar perfil"
-            if (btnEditarPerfil.textContent) {
-              btnEditarPerfil.textContent = 'Editar perfil';
-            }
-            inputs.forEach((input: HTMLInputElement) => {
-              input.readOnly = true;
-            });
-          }
-        });
-      }
-    });
-
     this.afAuth.authState.subscribe(user => {
       if (user) {
         this.userUID = user.uid;
@@ -73,8 +44,32 @@ export class PerfilUsuarioComponent {
           console.log(user);
           this.usuario = user;
         });
-
       }
+    });
+  }
+
+  editarPerfil() {
+    this.editando = !this.editando;
+  }
+
+  guardarCambios() {
+    this.editando = false;
+  }
+
+  cambiarModoEdicion() {
+    if (this.textoBoton === 'Editar perfil') {
+      this.textoBoton = 'Guardar cambios';
+      this.setReadOnly(false);
+    } else {
+      this.textoBoton = 'Editar perfil';
+      this.setReadOnly(true);
+    }
+  }
+
+  setReadOnly(valor: boolean) {
+    const inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('input');
+    inputs.forEach((input: HTMLInputElement) => {
+      input.readOnly = valor;
     });
   }
 
