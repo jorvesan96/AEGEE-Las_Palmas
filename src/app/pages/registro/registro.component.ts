2b21e751e625e6cab1ff7adc375cb12bd10e450c
AlejandroOrtegaG <alejandroog99@gmail.com>
2023-05-07 16:54:46 +0100

    Unificado nombre y apellidos

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators,AbstractControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from 'src/app/services/auth.service';
 import { registerVersion } from 'firebase/app';
@@ -8,6 +8,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import firebase from 'firebase/compat/app';
 import { onAuthStateChanged } from 'firebase/auth';
 import * as moment from 'moment';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -21,20 +22,19 @@ export class RegistroComponent implements OnInit {
   paso1Completo = false;
   constructor(private fb: FormBuilder, private http: HttpClient,
     private authService: AuthService, private db: AngularFireDatabase,
-    private fbs: AngularFirestore) { }
+    private fbs: AngularFirestore, private router: Router) { }
 
-    paises = [  { value: 'mx', label: 'México' },
-                { value: 'us', label: 'Estados Unidos' }, 
-                { value: 'es', label: 'España' }, 
-                { value: 'co', label: 'Colombia' }, 
-                { value: 'pe', label: 'Perú' }];
+  paises = [{ value: 'mx', label: 'México' },
+  { value: 'us', label: 'Estados Unidos' },
+  { value: 'es', label: 'España' },
+  { value: 'co', label: 'Colombia' },
+  { value: 'pe', label: 'Perú' }];
 
 
   ngOnInit() {
-    
+
     this.registroForm = this.fb.group({
       nombre: ['', Validators.required],
-      apellidos: ['', Validators.required],
       dni: ['', Validators.required],
       nacimiento: ['', Validators.required],
       pais: ['', Validators.required],
@@ -42,8 +42,8 @@ export class RegistroComponent implements OnInit {
       telefono: ['', Validators.required],
       correo: ['', [Validators.required, Validators.email]],
       repetirCorreo: ['', [Validators.required, Validators.email]],
-      contrasena: ['', [Validators.required, Validators.minLength(8)]],
-      repetirContrasena: ['', [Validators.required, Validators.minLength(8)]]
+      contrasena: ['', [Validators.required, Validators.minLength(6)]],
+      repetirContrasena: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
@@ -56,11 +56,16 @@ export class RegistroComponent implements OnInit {
     this.paso1Completo = true;
   }
 
+  get f() {
+    return this.registroForm.controls;
+  }
+
   enviarRegistro(registroForm: any) {
 
     this.authService.createUserWIthEmail(registroForm.value.correo, registroForm.value.contrasena)
       .then(() => {
         console.log("Usuario creado:")
+        this.createUser();
       })
       .catch((error) => {
         console.log("No se ha podido crear el usuario")
@@ -74,6 +79,9 @@ export class RegistroComponent implements OnInit {
 
     this.registroForm.value.nacimiento = fechaFormateada;
 
+  }
+  createUser() {
+
     const auth = firebase.auth();
     let userID !: string;
     const db = firebase.firestore();
@@ -81,18 +89,19 @@ export class RegistroComponent implements OnInit {
     auth.onAuthStateChanged((user) => {
       if (user) {
         userID = user.uid;
-        
-        const selectedPais = this.paises.find(pais => pais.value === registroForm.value.pais);
+
+        const selectedPais = this.paises.find(pais => pais.value === this.registroForm.value.pais);
         const paisLabel = selectedPais ? selectedPais.label : '';
-  
+
         // add label to form data before saving to Firestore
-        registroForm.value.pais = paisLabel;
+        this.registroForm.value.pais = paisLabel;
 
 
         const usuariosRef = db.collection('usuarios').doc(userID);
-        usuariosRef.set(registroForm.value) 
+        usuariosRef.set(this.registroForm.value)
           .then(docRef => {
             console.log("Usuario añadido");
+            this.router.navigate(["/"]);
           })
           .catch((error) => {
             console.log("No se ha podido añadir al usuario");
