87c1ab92363675ae2038130126eb7181d7cba81e
AlejandroOrtegaG <alejandroog99@gmail.com>
2023-05-09 12:28:43 +0100

    Nuevo proyecto

---
+++ AEGEE-LasPalmas/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AEGEE-LasPalmas',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
