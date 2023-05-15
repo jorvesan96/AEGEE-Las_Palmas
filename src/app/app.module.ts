445353f024ff0ebb50854487675cb60e7f1c8aed
AnaSantana016 <anasantanaojeda.16@gmail.com>
2023-05-15 09:30:44 +0200

    Prueba de inicio sesion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
@@ -11,10 +10,22 @@ import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage,getStorage } from '@angular/fire/storage';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
+
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/inicio-sesion/inicio-sesion.page.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-inicio-sesion',
+  templateUrl: './inicio-sesion.page.html',
+  styleUrls: ['./inicio-sesion.page.scss'],
+})
+export class InicioSesionPage{
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  correo: string | undefined;
+  canLogin: boolean = true;
+
+  loginForm = new FormGroup({
+    correo: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    ])),
+    contraseña: new FormControl('', Validators.compose([
+      Validators.minLength(5),
+      Validators.required,
+      Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+    ])),
+  });
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  login(loginForm: any): any {
+
+    const usuario = {
+      Correo: loginForm.correo,
+      Contraseña: loginForm.contraseña
+    }
+
+    this.authService.signInWithEmail(usuario.Correo, usuario.Contraseña)
+      .then(() => {
+        console.log("Autenticado con exito")
+        this.router.navigate(["/"]);
+      })
+      .catch((error) => {
+        this.canLogin = false;
+        console.log("Fallo el autenticar")
+        return false;
+      });
+
+  }
+
+}
