2b72cd394f42a10c36912ab53441efd6db1f269e
AnaSantana016 <anasantanaojeda.16@gmail.com>
2023-05-21 12:03:29 +0200

    Aun quedan cosas de inicio sesion

--- src/app/inicio-sesion/inicio-sesion.module.ts
+++ src/app/inicio-sesion/inicio-sesion.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 import { InicioSesionPageRoutingModule } from './inicio-sesion-routing.module';
-
 import { InicioSesionPage } from './inicio-sesion.page';
-
+import { HeaderComponent } from '../components/header/header.component';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    InicioSesionPageRoutingModule
+    InicioSesionPageRoutingModule,
+    ReactiveFormsModule
   ],
-  declarations: [InicioSesionPage]
+  declarations: [InicioSesionPage, HeaderComponent]
 })
 export class InicioSesionPageModule {}
--- src/app/inicio-sesion/inicio-sesion.page.ts
+++ src/app/inicio-sesion/inicio-sesion.page.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio-sesion',
   templateUrl: './inicio-sesion.page.html',
   styleUrls: ['./inicio-sesion.page.scss'],
 })
-export class InicioSesionPage implements OnInit {
+export class InicioSesionPage {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
-  ngOnInit() {
+  correo: string | undefined;
+  canLogin: boolean = true;
+
+  loginForm = new FormGroup({
+    correo: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    ])),
+    contraseña: new FormControl('', Validators.compose([
+      Validators.minLength(5),
+      Validators.required,
+      Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+    ])),
+  });
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  login(): void {
+
+
+  const correo = this.loginForm.value.correo ?? '';
+  const contraseña = this.loginForm.value.contraseña ?? '';
+
+    const usuario = {
+      Correo: correo,
+      Contraseña: contraseña
+    };
+
+    this.authService.signInWithEmail(usuario.Correo, usuario.Contraseña)
+      .then(() => {
+        console.log("Autenticado con éxito");
+        this.router.navigate(["/"]);
+      })
+      .catch((error) => {
+        this.canLogin = false;
+        console.log("Falló la autenticación");
+      });
   }
 
 }
