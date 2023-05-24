1571986b422955bc85ded8d4d8ed10b68c700af7
AnaSantana016 <anasantanaojeda.16@gmail.com>
2023-05-24 22:20:43 +0200

    Hecho el home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private router: Router){}
+
+  actividades: any[] = [];
+  actividadSeleccionada: any;
+
+  ngOnInit() {
+    this.recorrerColeccion();
+  }
+
+  async recorrerColeccion() {
+    const db = firebase.firestore();
+    const collectionRef = db.collection('actividades');
+    const querySnapshot = await collectionRef.get();
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      this.actividades.push(data);
+    });
+  }
+
+  navegarRegistroActividad() {
+    this.router.navigate(['/registro-actividad']);
+  }
 
 }
